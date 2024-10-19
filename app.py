e91a78243aaa47556317f54ec88470979ccad372
miasetya12 <miasetya12@gmail.com>
2024-10-19 19:52:16 +0800

    Add Api

--- app.py
+++ app.py
@@ -182,10 +182,11 @@ from flask_cors import CORS  # type: ignore
 from routes import configure_routes  # Import dari routes
 
 app = Flask(__name__)
-CORS(app)
+
 
 # Konfigurasi route
 configure_routes(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
+CORS(app)
